02a502b93bb9e057b8ab3928c40090725589941e
gongwenxian <gongwenxian123@163.com>
2018-01-17 16:24:03 +0800

    first commit

--- Helloword/Helloword/view.py
+++
@@ -1,8 +0,0 @@
-#from django.http import HttpResponse
-from django.shortcuts import render
-
-def abd(request):
-	#return HttpResponse("Hello!Gwx!")
-	content={}
-	content['hello']="Charming Prince~"
-	return render(request,'hello.html',content)
